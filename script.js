6fcec1e672c7c72431710b9723d2461fc6df6787
SrunTechsean <techseansrun@gmail.com>
2026-05-07 14:19:14 +0700

    feat(script): add edit icon into book log that's generted by js

--- script.js
+++ script.js
@@ -63,6 +63,25 @@ function renderLog() {
                                 <input ${checkedAttr} class="switch__input" type="checkbox">
                                 <span class="switch__slider"></span>
                             </label>
+
+                            <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="icon edit__icon"
+                            >
+                                    <title>Edit</title>
+                                    <path
+                                        d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+                                    />
+                                    <path d="m15 5 4 4" />
+                            </svg>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -140,10 +159,12 @@ logMenu.addEventListener("click", (e) => {
     // I put renderlog inside each cuz checkbox don't need to reRender
     if (remove) {
         removeEntries(entryID);
+
         // Re-render the book log
         renderLog();
     } else if (edit) {
         openEditModal(entryID);
+
         // Re-render the book log
         renderLog();
     } else if (checkbox) {
